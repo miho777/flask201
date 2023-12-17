4bab5d26dc51837aa57d2821fe58b71a242e7480
miho777 <naozumi.miho@emc.com>
2023-12-17 15:25:25 +0900

    Adjust OpenShift for Heroes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@
 # Import modules required for app
 import os
 from flask import Flask, render_template, request
-from models import get_photos, insert_photo, upload_photo
+from models import get_photos, insert_photo, upload_photo, delete_db
 
 # Create a Flask instance
 app = Flask(__name__)
@@ -15,20 +15,24 @@ app = Flask(__name__)
 ##### Define routes #####
 @app.route('/')
 def home():
-    album_photos = get_photos()							# Call function in 'models.py' to retrieve all photo's from database
+    album_photos = get_photos()						# Call function to retrieve all photo's from database in 'models.py' 
     return render_template('default.html',album_photos=album_photos,url="home")
 
 # This route accepts GET and POST calls
 @app.route('/upload', methods=['POST'])
 def upload():
-	insert_photo(request)								# Call function in 'models.py' to process the database transaction
-	upload_photo(request.files['photo'])				# Call function in 'models.py' to upload photo to ECS
-	return render_template('submit-photo.html')			# Return a page to inform the user of a successful upload
+	insert_photo(request)							# Call function to process the database transaction in 'models.py'
+	upload_photo(request.files['photo'])			# Call function to upload photo to ECS in 'models.py'
+	return render_template('submit-photo.html')		# Return a page to inform the user of a successful upload
 
 @app.route('/photo/<path:photo>')
 def photo(photo):
     return render_template('photo.html',photo=photo)
 
+@app.route('/deletedb')
+def delete():
+	delete_db()										# for Debug to delete documents of MongoDB in 'models.py'
+	return render_template('submit-photo.html')
 ##### Run the Flask instance, browse to http://<< Host IP or URL >>:5000 #####
 if __name__ == "__main__":
-	app.run(debug=False, host='0.0.0.0', port=int(os.getenv('PORT', '5000')), threaded=True)
+	app.run(host='0.0.0.0', port=int(os.getenv('PORT', '5000')), threaded=True)
--- models.py
+++ models.py
@@ -3,37 +3,37 @@
 # Import modules required for app
 import os
 import boto3
-import json
 from pymongo import MongoClient
 from werkzeug.utils import secure_filename
 from PIL import Image
 from config import ecs_test_drive, piper_mongodb
 
-### Local or Remote MongoDB instance
-# client = MongoClient("mongodb://USERNAME:PASSWORD@cluster0-shard-00-00.mzmpq.mongodb.net:27017,cluster0-shard-00-01.mzmpq.mongodb.net:27017,cluster0-shard-00-02.mzmpq.mongodb.net:27017/?ssl=true&replicaSet=atlas-4qm2w8-shard-0&authSource=admin&retryWrites=true&w=majority")
-
+### Remote MongoDB instance
 DB_USER = piper_mongodb['mongodb_user']
 
-#DB_PASSWORD = os.getenv('DB_PASSWORD','Need2SetDB_PASSWORD') 
-DB_PASSWORD = piper_mongodb['mongodb_password']  
+### Set DB password
+DB_PASSWORD = os.getenv('DB_PASSWORD','Need2SetDB_PASSWORD') 
+# DB_PASSWORD = piper_mongodb['mongodb_password']
+
+### Connect DB server
 db_arg = "mongodb://" + DB_USER + ":" + DB_PASSWORD + "@cluster0-shard-00-00.jnkoj.mongodb.net:27017,cluster0-shard-00-01.jnkoj.mongodb.net:27017,cluster0-shard-00-02.jnkoj.mongodb.net:27017/?ssl=true&replicaSet=atlas-10dctt-shard-0&authSource=admin&retryWrites=true&w=majority"
 client = MongoClient(db_arg)
 
-# Make sure this create your unique MongoDB database name######
+### Set DB name: Make sure this create your unique MongoDB database name ###
 #DB_NAME = os.getenv('DB_NAME','Need2SetDB_NAME')   ### e.g. P2023_NMiho  
 DB_NAME = piper_mongodb['mongodb_name']  
 
-# Get database connection with database name
+### Open DB with database name
 db = client[DB_NAME]
 
 # Remove any existing documents in photos collection
 # db.photos.delete_many({})   # Comment this line if you don't want to remove documents each time you start the app
 
-# Retrieve all photos records from database
+### Retrieve all photos records from database
 def get_photos():
     return db.photos.find({})
 
-# Insert form fields into database
+### Insert form fields into database
 def insert_photo(request):
     title = request.form['title']
     comments = request.form['comments']
@@ -52,8 +52,8 @@ def upload_photo(file):
     ecs_endpoint_url = ecs_test_drive['ecs_endpoint_url']
     # ecs_access_key_id = os.getenv('ECS_ID','Need2SetECS_ID')                ### Set Own ECS ID
     ecs_access_key_id = ecs_test_drive['ecs_access_key_id']
-    # ecs_secret_key = os.getenv('ECS_SECRET_KEY','Need2SetECS_SECRET_KEY')   ### Set Own ECS SECRET
-    ecs_secret_key = ecs_test_drive['ecs_secret_key']
+    ecs_secret_key = os.getenv('ECS_SECRET_KEY','Need2SetECS_SECRET_KEY')   ### Set Own ECS SECRET
+    # ecs_secret_key = ecs_test_drive['ecs_secret_key']
     ecs_bucket_name = ecs_test_drive['ecs_bucket_name']
 
     # Open a session with ECS using the S3 API
@@ -86,3 +86,7 @@ def upload_photo(file):
     # Delete the local files
     os.remove("uploads/" + filename)
     os.remove("uploads/" + thumbfile)
+
+###  for debug to delete all documents in MongoDB
+def delete_db():
+    db.photos.delete_many({}) 
